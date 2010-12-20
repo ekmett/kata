e797c506f35774948bf76bbcafc1d31db3dcfdfc
Edward Kmett <ekmett@gmail.com>
2010-12-20 15:43:25 -0500

    added maven.comonad.com, moved LayoutParsers to external project

--- project/build/kata.scala
+++ project/build/kata.scala
@@ -1,6 +1,14 @@
 import sbt._
 
 class kata(info: ProjectInfo) extends DefaultProject(info) {
-  val kiama = "com.googlecode" %% "kiama" % "1.0.1" from "http://scala-tools.org/repo-releases/com/googlecode/kiama_2.8.1/1.0.1/kiama_2.8.1-1.0.1.jar"
+  val comonadMaven = "comonad.com Maven Repository" at "http://maven.comonad.com/"
+
+  override def managedStyle = ManagedStyle.Maven
+  lazy val publishTo = Resolver.sftp("comonad.com Maven Repository sftp", "maven.comonad.com", "/home/ekmett/comonad.com/maven")
+
+  val layout = "com.comonad" %% "layout" % "0.1"
+
+  // workaround for a buggy pom file
+  val kiama = "com.googlecode" %% "kiama" % "1.0.1" from "http://nexus.scala-tools.org/content/groups/hosted/com/googlecode/kiama_2.8.1/1.0.1/kiama_2.8.1-1.0.1.jar"
 }
 
--- src/main/scala/kata/Parser.scala
+++ src/main/scala/kata/Parser.scala
@@ -1,5 +1,8 @@
 package kata
 
+import scala.util.parsing.combinator.LayoutParsers
+import scala.util.parsing.input.LayoutPosition
+
 object KataParser extends LayoutParsers {
   
 }
